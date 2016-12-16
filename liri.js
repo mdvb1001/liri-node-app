cc5fb896f8c7673053acb68c476d12f3e2ed2d2c
maxvanbel <maxvanbel@gmail.com>
2016-12-16 07:53:49 -0600

    get 3 APIs to work together with Inquirer

--- liri.js
+++ liri.js
@@ -1,96 +1,146 @@
-// TWITTER
-console.log('this is loaded');
-keys = require('./keys.js');
-
-// console.log(obj);
-var Twitter = require('twitter');
-var client = new Twitter({
-    consumer_key: keys.twitterKeys.consumer_key,
-    consumer_secret: keys.twitterKeys.consumer_secret,
-    access_token_key: keys.twitterKeys.access_token_key,
-    access_token_secret: keys.twitterKeys.access_token_secret
-});
-var params = {
-    screen_name: 'maxvanbel',
-    count: 5
-};
-client.get('statuses/user_timeline', params, function (error, tweets, response) {
-    if (!error) {
-        for (var i = 0; i < tweets.length; i++) {
-            console.log("\nTweet#" + i + ": " + tweets[i].text);
+// Load the NPM Package inquirer
+var inquirer = require('inquirer');
+var spotify = require('spotify');
+// Include the request npm package(Don 't forget to run "npm install request" in this folder first!)
+var request = require('request');
+var fs = require('fs');
+// Create a "Prompt" with a series of questions.
+inquirer.prompt([
+    // Here we create a basic text prompt.
+    {
+        type: "list",
+        message: "Tweets, Songs, Movies or Something?",
+        choices: ["tweets", "songs", "movies", "something"],
+        name: "searchType"
+    },
+    // Here we create a basic password-protected text prompt.
+    {
+        type: "confirm",
+        message: "my-tweets",
+        name: "tweets",
+        when: function(answers) {
+            return answers.searchType === "tweets";
+        }
+    }, {
+        type: "input",
+        message: "spotify-this-song",
+        name: "songs",
+        when: function(answers) {
+            return answers.searchType === "songs";
+        }
+    }, {
+        type: "input",
+        message: "movie-this",
+        name: "movies",
+        when: function(answers) {
+            return answers.searchType === "movies";
+        }
+    }, {
+        type: "confirm",
+        message: "do-what-it-say",
+        name: "something",
+        when: function(answers) {
+            return answers.searchType === "something";
         }
     }
+    // Once we are done with all the questions... "then" we do stuff with the answers
+    // In this case, we store all of the answers into a "user" object that inquirer makes for us. 
+]).then(function(search) {
+    // If we log that search as a JSON, we can see how it looks.
+    // console.log(JSON.stringify(search, null, 2));
+    if (search.searchType === "tweets") {
+        // TWITTER
+        console.log('this is loaded');
+        keys = require('./keys.js');
+        var Twitter = require('twitter');
+        var client = new Twitter({
+            consumer_key: keys.twitterKeys.consumer_key,
+            consumer_secret: keys.twitterKeys.consumer_secret,
+            access_token_key: keys.twitterKeys.access_token_key,
+            access_token_secret: keys.twitterKeys.access_token_secret
+        });
+        var params = {
+            screen_name: 'maxvanbel',
+            count: 20
+        };
+        client.get('statuses/user_timeline', params, function(error, tweets, response) {
+            if (!error) {
+                for (var i = 0; i < tweets.length; i++) {
+                    console.log("\nTweet #" + (i+1) + ": " + tweets[i].text);
+                }
+            }
+        });
+    } else if (search.searchType === "songs") {
+        // SPOTIFY 
+        // var type = process.argv[3];
+        // var response = process.argv[2];
+        var response = search.songs;
+        if (response.length === 0) {
+        	response = "the sign ace of base";
+        }
+        spotify.search({
+            type: 'track',
+            query: response
+        }, function(err, data) {
+            if (err) {
+                console.log('Error occurred: ' + err);
+                return;   
+            } else {
+                console.log("Song: " + data.tracks.items[0].name);
+                console.log("Artist: " + data.tracks.items[0].artists[0].name);
+                console.log("Preview: " + data.tracks.items[0].preview_url);
+                console.log("Album: " + data.tracks.items[0].album.name);
+            }
+            // console.log(data.tracks);
+        });
+    } else if (search.searchType === "movies") {
+        // OMBD 
+        // Store all of the arguments in an array 
+        // var nodeArgs = 
+        // var nodeArgs = process.argv;
+        // Create an empty variable for holding the movie name
+        var movieName = search.movies;
+        // Loop through all the words in the node argument
+        // And do a little for-loop magic to handle the inclusion of "+"s
+        // for (var i = 2; i < nodeArgs.length; i++) {
+        //     if (i > 2 && i < nodeArgs.length) {
+        //         movieName = movieName + "+" + nodeArgs[i];
+        //     } else {
+        //         movieName = movieName + nodeArgs[i];
+        //     }
+        // }
+        // Then run a request to the OMDB API with the movie specified 
+        var queryUrl = 'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&tomatoes=true&r=json';
+        // This line is just to help us debug against the actual URL.  
+        if (movieName.length === 0) {
+            movieName = "Mr. Nobody"; // this doesn't actually do anything...
+            queryUrl = 'http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&tomatoes=true&r=json';
+        }
+        console.log(queryUrl);
+        request(queryUrl, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                // Parse the body of the site and recover just the imdbRating
+                // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it). 
+                console.log("Title: " + JSON.parse(body).Title);
+                console.log("Release Year: " + JSON.parse(body).Year);
+                console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+                console.log("Country produced: " + JSON.parse(body).Country);
+                console.log("Language: " + JSON.parse(body).Language);
+                console.log("Plot: " + "\n" + JSON.parse(body).Plot);
+                console.log("Actors: " + JSON.parse(body).Actors);
+                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+                console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL);
+            }
+        });
+    } else if (search.searchType === "something") {
+    	fs.readFile("random.txt", "utf8", function(error, data){
+    		data = data.split(',');
+
+      	console.log(data);
+      	console.log("First element: " + data[0]);
+      	console.log("Second element: " + data[1]);
+        search.searchType = data[0];
+        	if (search.searchType === "movies") {};
+      });
+    }
 });
-// --------------------------------------->
-// SPOTIFY 
-// var type = process.argv[3];
-// var response = process.argv[2];
-// var spotify = require('spotify');
-// spotify.search({ type: 'track', query: response}, function(err, data) {
-//     if ( err ) {
-//         console.log('Error occurred: ' + err);
-//         return;
-//     }
-// 	console.log("Song: " + data.tracks.items[0].name);
-// 	console.log("Artist: " + data.tracks.items[0].artists[0].name);
-// 	console.log("Preview: " + data.tracks.items[0].preview_url);
-// 	console.log("Album: " + data.tracks.items[0].album.name);
-	// console.log(data.tracks);
-// var http = require('http');
-// });
-
-
-// Do something with 'data' 
-
-// 	   * Artist(s)
-//     * The song's name
-//     * A preview link of the song from Spotify
-//     * The album that the song is from
-
-// * if no song is provided then your program will default to
-//     * "The Sign" by Ace of Base
-// Client ID
-// 21eb58e4e4b44a92a536ae5c9da24551
-// Client Secret
-// 70afd877aa2c40c9834a3a3ef4fb9914
-// ----------------------------------------->
-// // OMBD 
-// // Include the request npm package(Don 't forget to run "npm install request" in this folder first!)
-// var request = require('request');
-// // Store all of the arguments in an array 
-// var nodeArgs = process.argv;
-// // Create an empty variable for holding the movie name
-// var movieName = "";
-// // Loop through all the words in the node argument
-// // And do a little for-loop magic to handle the inclusion of "+"s
-// for (var i = 2; i < nodeArgs.length; i++) {
-//     if (i > 2 && i < nodeArgs.length) {
-//         movieName = movieName + "+" + nodeArgs[i];
-//     } else {
-//         movieName = movieName + nodeArgs[i];
-//     }
-// }
-// // Then run a request to the OMDB API with the movie specified 
-// var queryUrl = 'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&tomatoes=true&r=json';
-// // This line is just to help us debug against the actual URL.  
-// if (nodeArgs.length === 2) {
-//     movieName = "Mr. Nobody"; // this doesn't actually do anything...
-//     queryUrl = 'http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&tomatoes=true&r=json';
-    
-// }
-// console.log(queryUrl);
-// request(queryUrl, function (error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//         // Parse the body of the site and recover just the imdbRating
-//         // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it). 
-//         console.log("Title: " + JSON.parse(body).Title);
-//         console.log("Release Year: " + JSON.parse(body).Year);
-//         console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-//         console.log("Country produced: " + JSON.parse(body).Country);
-//         console.log("Language: " + JSON.parse(body).Language);
-//         console.log("Plot: " + "\n" + JSON.parse(body).Plot);
-//         console.log("Actors: " + JSON.parse(body).Actors);
-//         console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
-//         console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL);
-//     }
-// });
---
+++ server.js
@@ -0,0 +1,14 @@
+ // --------------------------------------->
+            // var http = require('http');
+            // Do something with 'data' 
+            // 	   * Artist(s)
+            //     * The song's name
+            //     * A preview link of the song from Spotify
+            //     * The album that the song is from
+            // * if no song is provided then your program will default to
+            //     * "The Sign" by Ace of Base
+            // Client ID
+            // 21eb58e4e4b44a92a536ae5c9da24551
+            // Client Secret
+            // 70afd877aa2c40c9834a3a3ef4fb9914
+            // ----------------------------------------->
